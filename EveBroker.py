b659f8ab86601f3b7327c0f28d86de97a3377a0c
Christian Rickert <rc.login@icloud.com>
2021-05-29 00:59:10 -0600

    updated email address

--- EveBroker.py
+++ EveBroker.py
@@ -3,7 +3,7 @@
 """
 EveBroker
 Calculate your profit margin from buy to sell orders based on market skills and entity standings.
-Copyright (C) 2019 Christian Rickert <mail@crickert.de>
+Copyright (C) 2019 Christian Rickert <rc.email@icloud.com>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
